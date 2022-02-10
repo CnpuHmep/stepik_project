3442280482c2c29a1ec217ae97afa75102ff46b6
Aleksey Marchenko <marchenkorb@gmail.com>
2022-02-10 21:37:33 +0200

    Update шифр_цезаря.py

--- шифр_цезаря.py
+++ шифр_цезаря.py
@@ -1,21 +1,21 @@
 #   1040 ('А') по 1071 ('Я'), в нижнем регистре - с 1072 ('а') по 1103 ('я').
-def shifr_cezar(text):
+def shifr_cezar(text, n):
     text = list(text)
     new_text = []
     for i in range(len(text)):
         if 1040 <= ord(text[i]) <= 1071:
-            if (ord(text[i]) + 10) <= 1071:
-                new_text.append(chr(ord(text[i]) + 10))
+            if (ord(text[i]) + n) <= 1071:
+                new_text.append(chr(ord(text[i]) + n))
             else:
-                new_text.append(chr(((ord(text[i]) + 10) - 1071) + 1039))
+                new_text.append(chr(((ord(text[i]) + n) - 1071) + 1039))
         if 1072 <= ord(text[i]) <= 1103:
-            if (ord(text[i]) + 10) <= 1103:
-                new_text.append(chr(ord(text[i]) + 10))
+            if (ord(text[i]) + n) <= 1103:
+                new_text.append(chr(ord(text[i]) + n))
             else:
-                new_text.append(chr(((ord(text[i]) + 10) - 1103) + 1072))
+                new_text.append(chr(((ord(text[i]) + n) - 1103) + 1072))
         if not 1040 <= ord(text[i]) <= 1071 and not 1072 <= ord(text[i]) <= 1103:
             new_text.append(text[i])
     return new_text
 
 
-print(*(shifr_cezar('Блажен, кто верует, тепло ему на свете!')), sep='')
+print(*(shifr_cezar('Шсъцхр щмчжмщ йшм, нмтзж йшм лхшщзщг', -7)), sep='')
